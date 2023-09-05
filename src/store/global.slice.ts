af5488f461ead66459591ccd5f9f8107031aa6a4
Mohsin Ejaz <mejaz.bese21@gmail.com>
2023-09-05 16:52:06 +0500

    remove unused

--- src/store/global.slice.ts
+++ src/store/global.slice.ts
@@ -1,21 +1,4 @@
-import { Pizza, Topping } from "@src/models";
 
 export const GlobalSlice = {
-  isLoading: {
-    pizzas: true,
-    toppings: true,
-  },
-  validation: {
-    errors: [] as string[],
-    submitted: false,
-    reset: false,
-    skipRequired: false,
-  },
-  pizzas: [] as Pizza[],
-  toppings: [] as Topping[],
-  pizzaForm: {
-    id: undefined,
-    name: "",
-    toppings: [] as Topping[],
-  } as Pizza,
+  test: 'test',
 }
