4c9ae5721e973eb34a5a58ee1e50beae15c95e27
Mohsin Ejaz <mejaz.bese21@gmail.com>
2023-09-05 19:35:06 +0500

    change of slice

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,17 @@
 import ButtonWidget from '@src/components/Widgets/ButtonWidget';
-import { useGlobalContext } from '@src/hook/useGlobalContext';
+import { store$ } from '@src/store';
 import style from './HomePage.module.scss';
 
 export const HomePage = () => {
-  const [state, setters, reset] = useGlobalContext();
+  const store = store$.use();
   return (
-    <div className={style[state.className]}>
+    <div className={style[store.className]}>
       <h1>Heading 1</h1>
       <h2>Heading 2</h2>
       <h3>Heading 3</h3>
-      <p>Global context test: {state.className}</p>
-      <ButtonWidget kind="ok" onClick={() => { setters.className.set('blue') }}>CHANGE GLOBAL STATE</ButtonWidget>
-      <ButtonWidget kind="cancel" onClick={() => { reset() }}>RESET GLOBAL STATE</ButtonWidget>
+      <pre>{JSON.stringify(store, null, 2)}</pre>
+      <ButtonWidget kind="ok" onClick={() => { store$.className.set('gray') }}>GRAY</ButtonWidget>
+      <ButtonWidget kind="ok" onClick={() => { store$.className.set('green') }}>GREEN</ButtonWidget>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure perferendis voluptatibus perspiciatis eius soluta! Maiores voluptatibus libero porro id, ullam odio dicta amet pariatur in. Molestias facere voluptatum impedit esse distinctio aliquid! Non aperiam eveniet suscipit expedita ipsam ipsum ducimus minus commodi qui esse temporibus eum dicta, illo labore illum obcaecati nemo ut laudantium corporis dolore libero unde dolorem neque? Eaque sunt nulla, quisquam dicta, ea corporis quam atque aliquid laborum in debitis cum dolorem saepe nihil accusamus dolore, vitae natus sed consectetur maxime ipsum. Voluptates voluptatibus incidunt, nulla excepturi soluta corrupti officia sequi quidem sit eos nam et accusantium, aspernatur, vero asperiores molestiae! Ullam odio modi voluptatem sequi expedita laborum rerum, voluptas aliquam iure, quas voluptatum deserunt nam veritatis. Sed, eaque reprehenderit!</p>
     </div>
   );
--- src/store/global.slice.ts
+++ src/store/global.slice.ts
@@ -1,4 +1,4 @@
 
 export const GlobalSlice = {
-  test: 'test',
+  className: 'gray',
 }
