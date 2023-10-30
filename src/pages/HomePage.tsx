0cc624662ed4517810199150f1fd29cdc59bddff
Mohsin Ejaz <mejaz.bese21@gmail.com>
2023-10-30 14:50:04 +0500

    clean

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,74 @@
-import ButtonWidget from '@src/components/Widgets/ButtonWidget';
-import { store$ } from '@src/store';
+import classNames from 'classnames';
+import { useState } from 'react';
 import style from './HomePage.module.scss';
 export const HomePage = () => {
-  const className = store$.className.use();
+  const [keyword, setKeyword] = useState<String>('');
+  const [timeoutId, setTimeoutId] = useState<any>(null);
+  const keywords = [
+    'Alfa',
+    'Bravo',
+    'Charlie',
+    'Delta',
+    'Echo',
+    'Foxtrot',
+    'Golf',
+    'Hotel',
+    'India',
+    'Juliett',
+    'Kilo',
+    'Lima',
+    'Mike',
+    'November',
+    'Oscar',
+    'Papa',
+    'Quebec',
+    'Romeo',
+    'Sierra',
+    'Tango',
+    'Uniform',
+    'Victor',
+    'Whiskey',
+    'X-ray',
+    'Yankee',
+    'Zulu',
+  ]
+
+  const handleKeywordChange = (event) => {
+    const value = event.target.value;
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    const newTimeoutId = setTimeout(() => {
+      filterWords(value);
+    }, 500);
+    setTimeoutId(newTimeoutId);
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }
+
+  const filterWords = (keyword) => {
+    setKeyword(keyword);
+  }
+
+
+
   return (
-    <div className={style[className]}>
-      <h1>Class : {className}</h1>
-      <ButtonWidget kind="ok" onClick={() => { store$.className.set('gray') }}>GRAY</ButtonWidget>
-      <ButtonWidget kind="ok" onClick={() => { store$.className.set('green') }}>GREEN</ButtonWidget>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure perferendis voluptatibus perspiciatis eius soluta! Maiores voluptatibus libero porro id, ullam odio dicta amet pariatur in. Molestias facere voluptatum impedit esse distinctio aliquid! Non aperiam eveniet suscipit expedita ipsam ipsum ducimus minus commodi qui esse temporibus eum dicta, illo labore illum obcaecati nemo ut laudantium corporis dolore libero unde dolorem neque? Eaque sunt nulla, quisquam dicta, ea corporis quam atque aliquid laborum in debitis cum dolorem saepe nihil accusamus dolore, vitae natus sed consectetur maxime ipsum. Voluptates voluptatibus incidunt, nulla excepturi soluta corrupti officia sequi quidem sit eos nam et accusantium, aspernatur, vero asperiores molestiae! Ullam odio modi voluptatem sequi expedita laborum rerum, voluptas aliquam iure, quas voluptatum deserunt nam veritatis. Sed, eaque reprehenderit!</p>
+    <div className={style.container}>
+      <textarea className={style.textarea} onChange={handleKeywordChange} />
+      {
+        keywords.map((item, index) => {
+          return (
+            <div key={index} className={classNames(style.item, {
+              [style.item__highlight]: item.toLowerCase().trim().includes(keyword.toLowerCase().trim())
+            })}>
+              {item}
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
--- src/services/mock.service.ts
+++ src/services/mock.service.ts
@@ -1,28 +1,10 @@
-// import { Pizza } from '@src/models';
-// import { axios } from '@src/utils/axios';
-// function removePizza(payload: Pizza): Promise<Pizza> {
-//   return axios
-//     .POST<Pizza>({ url: `/pizzas/${payload.id}` })
-//     .then((response) => response?.data)
-// }
-// function updatePizza(payload: Pizza): Promise<Pizza> {
-//   return axios
-//     .PUT<Pizza>({ url: `/pizzas/${payload.id}`, body: payload })
-//     .then((response) => response?.data)
-// }
-// function createPizza(payload: Pizza): Promise<Pizza> {
-//   return axios
-//     .POST<Pizza>({ url: "/pizzas", body: payload })
-//     .then((response) => response?.data)
-// }
-// function getPizzas(): Promise<Pizza[]> {
-//   return axios
-//     .GET<Pizza[]>({ url: "/pizzas" })
-//     .then((response) => response?.data)
-// }
-// export const MockService = {
-//   removePizza,
-//   updatePizza,
-//   createPizza,
-//   getPizzas,
-// };
+import { axios } from '@src/utils/axios';
+function filterWords(payload): Promise<any> {
+  return axios
+    .GET<any>({ url: `/filter/${payload}` })
+    .then((response) => response?.data)
+}
+
+export const MockService = {
+  filterWords,
+};
